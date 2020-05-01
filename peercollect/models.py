d9c42f5e4bc877d23b03cb0a66dd76f2349781a8
Faidon Liambotis <faidon@wikimedia.org>
2020-05-02 01:01:25 +0300

    Modernize peercollect (minor)

--- peercollect/models.py
+++ peercollect/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 from django_peeringdb.models.concrete import NetworkIXLan
 
@@ -10,7 +9,7 @@ class Peering(models.Model):
     )
     router = models.CharField(max_length=255, db_index=True)
 
-    def __str__(self):
-        return "Peering with {} on {}".format(
-            self.netixlan.net.name, self.netixlan.ixlan.ix.name
-        )
+    def __repr__(self):
+        peer_name = self.netixlan.net.name
+        ixp_name = self.netixlan.ixlan.ix.name
+        return f"Peering with {peer_name} at {ixp_name}"
