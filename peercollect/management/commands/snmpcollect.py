d4549cf8c06a5399ae7c8a5406a65c448aa628ac
Faidon Liambotis <faidon@wikimedia.org>
2020-05-02 20:34:30 +0300

    Update SNMP collect with current status of IPv6

--- peercollect/management/commands/snmpcollect.py
+++ peercollect/management/commands/snmpcollect.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 from django.conf import settings
-from django.utils.six import iteritems
 from snimpy.manager import Manager as M
 from snimpy.manager import load
 from snimpy.snmp import SNMPException
@@ -18,15 +17,28 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        # only BGP4-MIB for now, there were snimpy troubles with Juniper's BGP
-        # v2 MIB:
-        # http://puck.nether.net/pipermail/juniper-nsp/2017-March/034067.html
+        # BGP4-MIB is IPv4-only. For IPv6, there are:
+        #   * draft-ietf-idr-bgp4-mibv2-15 (IETF, but expired)
+        #   * BGP4-V2-MIB-JUNIPER (Juniper)
+        #   * BGP4V2-MIB (Foundry/Brocade)
+        #   * CISCO-BGP4-MIB (Cisco)
+        #   * ARISTA-BGP4V2-MIB (Arista)
+        #   * FORCE10-BGP4-V2-MIB (Force10)
+        #
+        # Juniper was found to be buggy, and needs:
+        #   set protocols bgp snmp-options emit-inet-address-length-in-oid
+        # See:
+        # https://puck.nether.net/pipermail/juniper-nsp/2017-March/034067.html
+        # and the resulting PR 1265504 (reported from this codebase!)
+        #
+        # As of May 2020, there are still issues with parsing, needing
+        # additional code to parse (address type, address) tuples. snimpy#90.
         #
         # This effectively means only IPv4 matches, which is probably okay for
         # this use case (and probably this decade).
         load("BGP4-MIB")
 
-        for router, community in iteritems(settings.SNMP_ROUTERS):
+        for router, community in settings.SNMP_ROUTERS.items():
             m = M(host=router, community=community, version=2)
 
             netixlans = []
@@ -34,7 +46,7 @@ class Command(BaseCommand):
 
             try:
                 peers = m.bgpPeerRemoteAs
-                for ip, asn in iteritems(peers):
+                for ip, asn in peers.iteritems():
                     try:
                         netixlan = NetworkIXLan.objects.get(ipaddr4=ip, asn=asn)
                         netixlans.append(netixlan)
